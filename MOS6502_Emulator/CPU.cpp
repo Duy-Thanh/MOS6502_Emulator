dcf9754cd3c6d5d204b3b317913f4418fedbe383
nekkochan <nekkochan@gmail.com>
2024-12-13 23:01:13 +0700

    Update CPU.cpp and CPU.h

--- MOS6502_Emulator/CPU.cpp
+++ MOS6502_Emulator/CPU.cpp
@@ -11,12 +11,7 @@ namespace MOS6502 {
 			this->memory = new MOS6502_Memory();
 		}
 
-		this->cpu->A = 0x00;
-		this->cpu->P = 0x00;
-		this->cpu->PC = 0x0200;
-		this->cpu->S = 0xFF;
-		this->cpu->X = 0x00;
-		this->cpu->Y = 0x00;
+		this->MOS6502_CPU_Reset();
 	}
 
 	MOS6502_CPU::~MOS6502_CPU() {
@@ -33,16 +28,15 @@ namespace MOS6502 {
 	void MOS6502_CPU::MOS6502_CPU_Reset() {
 		if (this->cpu) {
 			try {
-				delete this->cpu;
-
-				this->cpu = new CPU();
-
 				this->cpu->A = 0x00;
-				this->cpu->P = 0x00;
-				this->cpu->PC = 0x0200;
-				this->cpu->S = 0xFF;
+				this->cpu->P = UNUSED;
+				this->cpu->S = 0xFD;
 				this->cpu->X = 0x00;
 				this->cpu->Y = 0x00;
+
+				this->cpu->PC = this->MOS6502_CPU_MemReadWord(0xFFFC);
+
+				this->cycles = 0;
 			}
 			catch (const std::exception& e) {
 				std::cout << "EXCEPTION: " << e.what() << std::endl;
@@ -50,21 +44,23 @@ namespace MOS6502 {
 		}
 	}
 
-	void MOS6502_CPU::MOS6502_CPU_SetFlags(uint8_t& status, uint8_t flag, bool condition) {
-		if (condition) status |= flag;
-		else status &= ~flag;
-
-		this->cpu->P = MOS6502_CPU_STATUS_FLAGS::UNUSED;
+	// Status flag operations
+	void MOS6502_CPU::MOS6502_CPU_SetFlags(MOS6502_CPU_STATUS_FLAGS flag, bool value) {
+		if (value)
+			SET_FLAG(this->cpu->P, flag);
+		else
+			CLEAR_FLAG(this->cpu->P, flag);
 	}
 
-	bool MOS6502_CPU::MOS6502_CPU_GetFlags(uint8_t status, uint8_t flag) {
-		return (status & flag) != 0;
+	bool MOS6502_CPU::MOS6502_CPU_GetFlags(MOS6502_CPU_STATUS_FLAGS flag) {
+		return GET_FLAG(this->cpu->P, flag);
 	}
 
 	uint8_t MOS6502_CPU::MOS6502_CPU_get7BitStatus(uint8_t status) {
 		return status & 0x7F;				// Mask out the 8th bit (bit 7)
 	}
 
+	// Memory operations
 	uint8_t MOS6502_CPU::MOS6502_CPU_MemRead(uint16_t addr) {
 		return this->memory->Read(addr);
 	}
@@ -72,4 +68,78 @@ namespace MOS6502 {
 	void MOS6502_CPU::MOS6502_CPU_MemWrite(uint16_t addr, uint8_t value) {
 		this->memory->Write(addr, value);
 	}
-}
+
+	uint16_t MOS6502_CPU::MOS6502_CPU_MemReadWord(uint16_t addr) {
+		uint16_t low = this->MOS6502_CPU_MemRead(addr);
+		uint16_t high = this->MOS6502_CPU_MemRead(addr + 1);
+
+		return (high << 8) | low;
+	}
+
+	void MOS6502_CPU::MOS6502_CPU_MemWriteWord(uint16_t addr, uint16_t value) {
+		this->MOS6502_CPU_MemWrite(addr, value & 0xFF);
+		this->MOS6502_CPU_MemWrite(addr + 1, (value >> 8) & 0xFF);
+	}
+
+	// Private functions
+	void MOS6502_CPU::MOS6502_CPU_Push(uint8_t value) {
+		this->MOS6502_CPU_MemWrite(0x0100 + this->cpu->S, value);
+		this->cpu->S--;
+	}
+
+	uint8_t MOS6502_CPU::MOS6502_CPU_Pop() {
+		this->cpu->S++;
+		return this->MOS6502_CPU_MemRead(0x0100 + this->cpu->S);
+	}
+
+	void MOS6502_CPU::MOS6502_CPU_PushWord(uint16_t value) {
+		this->MOS6502_CPU_Push((value >> 8) & 0xFF);
+		this->MOS6502_CPU_Push(value & 0xFF);
+	}
+
+	uint16_t MOS6502_CPU::MOS6502_CPU_PopWord() {
+		uint16_t low = this->MOS6502_CPU_Pop();
+		uint16_t high = this->MOS6502_CPU_Pop();
+
+		return (high << 8) | low;
+	}
+
+	uint8_t MOS6502_CPU::MOS6502_CPU_Fetch() {
+		uint8_t data = this->MOS6502_CPU_MemRead(this->cpu->PC);
+		this->cpu->PC++;
+		return data;
+	}
+
+	uint16_t MOS6502_CPU::MOS6502_CPU_FetchWord() {
+		uint16_t word = this->MOS6502_CPU_MemReadWord(this->cpu->PC);
+		this->cpu->PC += 2;
+		return word;
+	}
+
+	// Core functions
+	void MOS6502_CPU::MOS6502_CPU_Clock() {
+		if (this->cycles_remaining == 0) {
+			this->opcode = this->MOS6502_CPU_Fetch();
+
+			this->cycles_remaining = this->MOS6502_CPU_GetInstructionCycles(this->opcode);
+
+			this->MOS6502_CPU_ExecuteInstruction(this->opcode);
+		}
+	}
+
+	uint8_t MOS6502_CPU::MOS6502_CPU_GetInstructionCycles(uint8_t opcode) {
+		return 2;
+	}
+
+	void MOS6502_CPU::MOS6502_CPU_ExecuteInstruction(uint8_t opcode) {
+		switch (opcode) {
+		default:
+			std::cout << "Unknown opcode: 0x" << std::hex << (int)opcode << std::endl;
+			break;
+		}
+	}
+
+	bool MOS6502_CPU::MOS6502_CPU_Complete() {
+		return this->cycles_remaining == 0;
+	}
+}													 
--- MOS6502_Emulator/CPU.h
+++ MOS6502_Emulator/CPU.h
@@ -5,6 +5,10 @@
 #include <iostream>
 #include <unordered_map>
 
+#define SET_FLAG(P, F)			((P) |= (F))
+#define CLEAR_FLAG(P, F)		((P) &= ~(F))
+#define GET_FLAG(P, F)			((P) & (F))
+
 typedef struct _CPU {
 	uint8_t A;					// Accumulator (8-bit register)
 	uint8_t P;					// Status register (7-bit register)
@@ -14,16 +18,28 @@ typedef struct _CPU {
 	uint8_t Y;					// Another index register (8-bit register)
 } CPU, * PCPU;
 
+//enum MOS6502_CPU_STATUS_FLAGS {
+//	DEFAULT		= 0x00,			// DEFAULT_CPU_STATUS_FLAGS
+//	CARRY		= 0x01,			// Bit 0
+//	ZERO		= 0x02,			// Bit 1
+//	INTERRUPT	= 0x04,			// Bit 2
+//	DECIMAL		= 0x08,			// Bit 3
+//	BREAK		= 0x10,			// Bit 4
+//	UNUSED		= 0x20,			// Bit 5 (always 1 or ignored)
+//	FOVERFLOW	= 0x40,			// Bit 6
+//	NEGATIVE	= 0x80			// Bit 7
+//};
+
 enum MOS6502_CPU_STATUS_FLAGS {
-	DEFAULT		= 0x00,			// DEFAULT_CPU_STATUS_FLAGS
-	CARRY		= 0x01,			// Bit 0
-	ZERO		= 0x02,			// Bit 1
-	INTERRUPT	= 0x04,			// Bit 2
-	DECIMAL		= 0x08,			// Bit 3
-	BREAK		= 0x10,			// Bit 4
-	UNUSED		= 0x20,			// Bit 5 (always 1 or ignored)
-	FOVERFLOW	= 0x40,			// Bit 6
-	NEGATIVE	= 0x80			// Bit 7
+	DEFAULT		= (0 << 0),			// DEFAULT_CPU_STATUS_FLAGS
+	CARRY		= (1 << 0),			// Bit 0
+	ZERO		= (1 << 1),			// Bit 1
+	INTERRUPT	= (1 << 2),			// Bit 2
+	DECIMAL		= (1 << 3),			// Bit 3
+	BREAK		= (1 << 4),			// Bit 4
+	UNUSED		= (1 << 5),			// Bit 5 (always 1 or ignored)
+	FOVERFLOW	= (1 << 6),			// Bit 6
+	NEGATIVE	= (1 << 7)			// Bit 7
 };
 
 namespace MOS6502 {
@@ -32,16 +48,40 @@ namespace MOS6502 {
 		PCPU cpu;
 		MOS6502_Memory* memory;
 		unsigned long cycles;
+
+		uint8_t opcode = 0x00;										// Current instruction opcodes
+		uint8_t cycles_remaining = 0;
+		bool completed = true;
+
+		uint8_t MOS6502_CPU_Fetch();
+		uint16_t MOS6502_CPU_FetchWord();
+		void MOS6502_CPU_Push(uint8_t value);
+		uint8_t MOS6502_CPU_Pop();
 	public:
 		MOS6502_CPU();
 		~MOS6502_CPU();
 
+		// Memory operations
 		uint8_t MOS6502_CPU_MemRead(uint16_t addr);
 		void MOS6502_CPU_MemWrite(uint16_t addr, uint8_t value);
+		uint16_t MOS6502_CPU_MemReadWord(uint16_t addr);
+		void MOS6502_CPU_MemWriteWord(uint16_t addr, uint16_t value);
 
+		// Core functions
 		void MOS6502_CPU_Reset();
-		void MOS6502_CPU_SetFlags(uint8_t& status, uint8_t flag, bool condition);
-		bool MOS6502_CPU_GetFlags(uint8_t status, uint8_t flag);
+		void MOS6502_CPU_Clock();
+		bool MOS6502_CPU_Complete();
+
+		uint8_t MOS6502_CPU_GetInstructionCycles(uint8_t opcode);
+		void MOS6502_CPU_ExecuteInstruction(uint8_t opcode);
+
+		// Stack operations
+		void MOS6502_CPU_PushWord(uint16_t value);
+		uint16_t MOS6502_CPU_PopWord();
+
+		// Status flag operations
+		void MOS6502_CPU_SetFlags(MOS6502_CPU_STATUS_FLAGS flag, bool value);
+		bool MOS6502_CPU_GetFlags(MOS6502_CPU_STATUS_FLAGS flag);
 		uint8_t MOS6502_CPU_get7BitStatus(uint8_t status);
 	};
 }
