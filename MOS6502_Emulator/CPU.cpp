01fbfb55c04576ebb092d13d0765e6d2e90c0c62
nekkochan <nekkochan@gmail.com>
2024-12-14 21:36:48 +0700

    Update CPU.cpp and CPU.h

--- MOS6502_Emulator/CPU.cpp
+++ MOS6502_Emulator/CPU.cpp
@@ -3,31 +3,48 @@
 namespace MOS6502 {
 	MOS6502_CPU::MOS6502_CPU() {
 		// Initialize CPU register (Important)
+		std::cout << "CPU: Trying initialize CPU" << std::endl;
+
 		if (this->cpu == nullptr) {
+			std::cout << "CPU: CPU is not initialized before. Initializing CPU..." << std::endl;
 			this->cpu = new CPU();
+			std::cout << "CPU: CPU is initialized addr=0x" << std::hex << (uint32_t)this->cpu << " var_addr=0x" << std::hex << (uint32_t)&this->cpu << std::endl;
 		}
+
+		std::cout << "MEM: CPU is requesting initialize the memory" << std::endl;
+		std::cout << "MEM: Trying initialize memory" << std::endl;
 
 		if (this->memory == nullptr) {
+			std::cout << "MEM: Memory is not initialized before. Initializing memory..." << std::endl;
 			this->memory = new MOS6502_Memory();
+			std::cout << "MEM: Memory is initialized addr=0x" << std::hex << (uint32_t)this->memory << " var_addr=0x" << std::hex << (uint32_t)&this->memory << std::endl;
 		}
 
+		std::cout << "CPU: CPU Reset requested" << std::endl;
 		this->MOS6502_CPU_Reset();
 	}
 
 	MOS6502_CPU::~MOS6502_CPU() {
+		std::cout << "CPU: Shutting down the CPU" << std::endl;
 		if (this->cpu) {
+			std::cout << "CPU: CPU was initialized. Continue to shutdown the CPU" << std::endl;
 			try {
+				std::cout << "CPU: Shutting down the CPU addr=0x" << std::hex << (uint32_t)this->cpu << " var_addr=0x" << (uint32_t)&this->cpu << std::endl;
 				delete this->cpu;
+				std::cout << "CPU: CPU shutdown completed!" << std::endl;
 			}
 			catch (const std::exception& e) {
-				std::cout << "EXCEPTION: " << e.what() << std::endl;
+				std::cout << "CPU: EXCEPTION: " << e.what() << std::endl;
 			}
 		}
 	}
 
 	void MOS6502_CPU::MOS6502_CPU_Reset() {
+		std::cout << "CPU: CPU is resetting" << std::endl;
 		if (this->cpu) {
+			std::cout << "CPU: CPU was initialized. Continuing to reset the CPU" << std::endl;
 			try {
+				std::cout << "CPU: Setting up the CPU..." << std::endl;
 				this->cpu->A = 0x00;
 				this->cpu->P = UNUSED;
 				this->cpu->S = 0xFD;
@@ -37,9 +54,12 @@ namespace MOS6502 {
 				this->cpu->PC = this->MOS6502_CPU_MemReadWord(0xFFFC);
 
 				this->cycles = 0;
+
+				std::cout << "CPU: Setting up the CPU was successfully!" << std::endl;
+
 			}
 			catch (const std::exception& e) {
-				std::cout << "EXCEPTION: " << e.what() << std::endl;
+				std::cout << "CPU: EXCEPTION: " << e.what() << std::endl;
 			}
 		}
 	}
@@ -72,8 +92,12 @@ namespace MOS6502 {
 	}
 
 	uint16_t MOS6502_CPU::MOS6502_CPU_MemReadWord(uint16_t addr) {
+		std::cout << "CPU: MemReadWord at addr 0x" << std::hex << addr << std::endl;
+
 		uint16_t low = this->MOS6502_CPU_MemRead(addr);
 		uint16_t high = this->MOS6502_CPU_MemRead(addr + 1);
+
+
 
 		return (high << 8) | low;
 	}
@@ -109,7 +133,10 @@ namespace MOS6502 {
 	uint8_t MOS6502_CPU::MOS6502_CPU_Fetch() {
 		std::cout << "CPU: Fetch instruction" << std::endl;
 		uint8_t data = this->MOS6502_CPU_MemRead(this->cpu->PC);
+		std::cout << "CPU: Fetch(): PC = 0x" << std::hex << (int)this->cpu->PC << std::endl;
 		this->cpu->PC++;
+		std::cout << "CPU: Fetch(): Increasing PC register with 0x1" << std::endl;
+		std::cout << "CPU: Fetch(): Current PC = 0x" << std::hex << (int)this->cpu->PC << std::endl;
 		return data;
 	}
 
@@ -131,14 +158,15 @@ namespace MOS6502 {
 	}
 
 	uint8_t MOS6502_CPU::MOS6502_CPU_GetInstructionCycles(uint8_t opcode) {
-		std::cout << "GetInstructionCycles: opcode = 0x" << std::hex << (int)opcode << std::endl;
+		std::cout << "CPU: GetInstructionCycles: opcode = 0x" << std::hex << (int)opcode << std::endl;
 		return 2;
 	}
 
 	void MOS6502_CPU::MOS6502_CPU_ExecuteInstruction(uint8_t opcode) {
+		std::cout << "CPU: ExecuteInstruction: opcode = 0x" << std::hex << (int)opcode << std::endl;
 		switch (opcode) {
 		default:
-			std::cout << "Unknown opcode: 0x" << std::hex << (int)opcode << std::endl;
+			std::cout << "CPU: ExecutionInstruction: Unknown opcode: 0x" << std::hex << (int)opcode << std::endl;
 			break;
 		}
 	}
--- MOS6502_Emulator/Main.cpp
+++ MOS6502_Emulator/Main.cpp
@@ -5,14 +5,33 @@
 #include <thread>
 
 int main() {
+	std::cout << std::endl << std::endl;
+	std::this_thread::sleep_for(std::chrono::seconds(2));
+
+	std::cout << "--------------------------------------------------------------------------------" << std::endl;
+	std::cout << "\n\n";
+	std::cout << "                            MOS6502 EMULATOR                                    " << std::endl;
+	std::cout << "\n\n";
+	std::cout << "                     MOS6502 COMPLETE SYSTEM EMULATOR                           " << std::endl;
+	std::cout << "\n\n";
+	std::cout << "              COPYRIGHT (C) 2024 NEKKOCHAN. ALL RIGHT RESERVED.                 " << std::endl;
+	std::cout << "\n\n";
+	std::cout << "--------------------------------------------------------------------------------" << std::endl;
+	std::cout << "\n\n";
+
 	MOS6502::MOS6502_CPU cpu;
 
+	std::cout << "MAIN: cpu_var_addr=0x" << std::hex << (uint32_t) & cpu << std::endl;
+
 	bool running = true;
+
+	std::cout << "MAIN: State is set to RUNNING" << std::endl;
+
 	while (running) {
 		cpu.MOS6502_CPU_Clock();
 
 		if (cpu.MOS6502_CPU_Complete()) {
-
+			// TODO
 		}
 	}
 
