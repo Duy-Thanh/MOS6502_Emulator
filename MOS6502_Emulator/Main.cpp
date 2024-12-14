5801e2f5ba7168144de3c415d6f801ca10b8bb23
nekkochan <nekkochan@gmail.com>
2024-12-14 08:56:37 +0700

    Update CPU.cpp and CPU.h

--- MOS6502_Emulator/Main.cpp
+++ MOS6502_Emulator/Main.cpp
@@ -1,7 +1,20 @@
 #include "CPU.h"
 
 #include <iostream>
+#include <chrono>
+#include <thread>
 
 int main() {
+	MOS6502::MOS6502_CPU cpu;
+
+	bool running = true;
+	while (running) {
+		cpu.MOS6502_CPU_Clock();
+
+		if (cpu.MOS6502_CPU_Complete()) {
+
+		}
+	}
+
 	return 0;
 }
